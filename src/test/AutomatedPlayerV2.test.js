9a7c8a96518cbd860a02e8433b048cf7a45e12b4
OllieRumbol <otgb97@gmail.com>
2021-02-14 11:32:47 +0000

    basic test of minimax

--- src/test/AutomatedPlayerV2.test.js
+++ src/test/AutomatedPlayerV2.test.js
@@ -1,4 +1,23 @@
-import { evaluate } from '../logic/automatedPlayerV2'
+import { evaluate, minimax } from '../logic/automatedPlayerV2'
+
+test("Check evaluate", () => {
+    let board = [
+        [2, 0, 2, 0, 2, 0, 2, 0],
+        [0, 2, 0, 2, 0, 2, 0, 2],
+        [2, 0, 2, 0, 2, 0, 2, 0],
+        [0, 1, 0, 5, 0, 5, 0, 5],
+        [5, 0, 5, 0, 5, 0, 5, 0],
+        [0, 5, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+    ];
+
+    let result = minimax(board, 2, true);
+
+    expect(result[1].nextHeight).toEqual(4);
+    expect(result[1].nextWidth).toEqual(0);
+    expect(result[1].takes.length).toEqual(1);
+});
 
 test("Check evaluate", () => {
     let board = [
