eb28df83d0e6f87e2f1ba841557a7cb2c501cb3b
OllieRumbol <otgb97@gmail.com>
2021-02-18 09:49:03 +0000

    Keep styling for helper sections

--- src/components/home/Pieces.js
+++ src/components/home/Pieces.js
@@ -7,12 +7,12 @@ export default function Pieces(props) {
     return (
         <div>
             <Row className="p-3">
-                <div className="circle red center"></div>
+                <div className="circle2 red center"></div>
                 <h3 className="p-4">Single</h3>
             </Row>
             <br></br>
             <Row className="p-3">
-                <div className="circle red center">K</div>
+                <div className="circle2 red center">K</div>
                 <h3 className="p-4">King</h3>
             </Row>
         </div>
