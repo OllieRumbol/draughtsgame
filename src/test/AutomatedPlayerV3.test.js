9d7fa4f0b265b7f394efa98c0a16fbb6eda302ae
OllieRumbol <otgb97@gmail.com>
2021-03-01 11:57:21 +0000

    automated tests

---
+++ src/test/AutomatedPlayerV3.test.js
@@ -0,0 +1,50 @@
+import {
+    minimax,
+    evaluate,
+    GetAvailableBoards,
+    FindAvailableTakeMoves,
+    checkKingTake,
+    checkTakeUp,
+    checkTakeDown,
+    FindAvailableMoves,
+    checkMoveDownRight,
+    checkMoveDownLeft,
+    checkMoveUpRight,
+    checkMoveUpLeft
+}
+
+    from '../logic/automatedPlayerV3';
+
+test('evaluate method', () => {
+    let board = [
+        [0, 2, 0, 2, 0, 2, 0, 2],
+        [2, 0, 2, 0, 2, 0, 2, 0],
+        [0, 2, 0, 2, 0, 2, 0, 2],
+        [5, 0, 5, 0, 5, 0, 5, 0],
+        [0, 5, 0, 5, 0, 5, 0, 5],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+    ];
+
+    let result = evaluate(board);
+
+    expect(result).toEqual(0);
+})
+
+test('evaluate method counters to be taken', () => {
+    let board = [
+        [0, 2, 0, 2, 0, 2, 0, 2],
+        [2, 0, 2, 0, 2, 0, 2, 0],
+        [0, 2, 0, 2, 0, 2, 0, 2],
+        [5, 0, 1, 0, 5, 0, 5, 0],
+        [0, 5, 0, 5, 0, 5, 0, 5],
+        [1, 0, 1, 0, 1, 0, 5, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1, 0],
+    ];
+
+    let result = evaluate(board);
+
+    expect(result).toEqual(1);
+})
