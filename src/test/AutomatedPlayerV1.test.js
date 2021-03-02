9e02ef8a135adcb93a7c66535f81fe185a438384
OllieRumbol <otgb97@gmail.com>
2021-03-02 18:12:33 +0000

    Upated tests

--- src/test/AutomatedPlayerV1.test.js
+++ src/test/AutomatedPlayerV1.test.js
@@ -1,18 +1,18 @@
-import { checkMoveUpLeft } from '../logic/automatedPlayerV1'
+import { pickMoveForPlayer2 } from '../logic/automatedPlayerV1'
 
 test("Check move left function", () => {
     let board = [
-        [2, 0, 2, 0, 2, 0, 2, 0],
         [0, 2, 0, 2, 0, 2, 0, 2],
         [2, 0, 2, 0, 2, 0, 2, 0],
-        [0, 1, 0, 5, 0, 5, 0, 5],
+        [0, 2, 0, 2, 0, 2, 0, 2],
         [5, 0, 5, 0, 5, 0, 5, 0],
-        [0, 1, 0, 1, 0, 1, 0, 1],
+        [0, 5, 0, 5, 0, 5, 0, 5],
         [1, 0, 1, 0, 1, 0, 1, 0],
         [0, 1, 0, 1, 0, 1, 0, 1],
-    ];
+        [1, 0, 1, 0, 1, 0, 1, 0],
+    ];;
 
-    let result = checkMoveUpLeft(board, 2, 0);
+    let result = pickMoveForPlayer2(board);
 
-    expect(result).toEqual(false);
+    expect(result.takes).toEqual([]);
 });
