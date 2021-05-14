d1c1999d8a832ea545b622f880f3b72eaea2f6cc
OllieRumbol <otgb97@gmail.com>
2021-05-14 19:00:10 +0100

    adding async

--- src/logic/api.js
+++ src/logic/api.js
@@ -15,30 +15,30 @@ async function post(url, data) {
     return await response.json();
 }
 
-function GetAutomatedPlayerNextMove(version, board, depth) {
+async function GetAutomatedPlayerNextMove(version, board, depth) {
     //let url = 'http://localhost:7071/api/GetNextMove'
     let url = 'https://draughtsgamefunction20210321121511.azurewebsites.net/api/GetNextMove';
     let data = { Version: version, Board: board, Depth: depth, Player: 2 }
-    return post(url, data).then(res => {
-        if(res.successful){
-            return res.nextMove;
-        }
-        else{
-            throw res.errorMessage;
-        }
-    });
+    const result = await post(url, data)
+
+    if (result.successful) {
+        return result.nextMove;
+    }
+    else {
+        throw result.errorMessage;
+    }
 }
 
-function GetPlayerTips(board, tipFor){
+async function GetPlayerTips(board, tipFor) {
     //let url = 'http://localhost:7071/api/GetPotentialMoves';
     let url = 'https://draughtsgamefunction20210321121511.azurewebsites.net/api/GetPotentialMoves';
-    let data = { Board: board, TipFor: tipFor};
-    return post(url, data).then(res => {
-        if(res.successful){
-            return res.potentialMoves;
-        }
-        else{
-            throw res.errorMessage;
-        }
-    });
+    let data = { Board: board, TipFor: tipFor };
+    const result = await post(url, data);
+
+    if (result.successful) {
+        return result.potentialMoves;
+    }
+    else {
+        throw result.errorMessage;
+    }
 }
